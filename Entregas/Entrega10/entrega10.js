72844896ffac5e4b60d3af2c3e7d68965a558d39
Victor <victordpc@gmail.com>
2018-10-24 17:34:43 +0200

    Entrega 10 javascript

---
+++ Entregas/Entrega10/entrega10.js
@@ -0,0 +1,39 @@
+// Ejercicio 8
+
+class Figura {
+    constructor(x, y) {
+        this._x = x;
+        this._y = y;
+        this._color = "#000000";
+    }
+    set color(color) {
+        if (/[#]{1}[a-fA-F0-9]{6}/.test(color)) {
+            this._color = color;
+        }
+    }
+    pintar() {
+        console.log("Nos movemos a la posición (" + str(this._x) + ", " + str(this._y) + ") Cogemos la pintura de color " + this._color)
+    }
+    esblanca() {
+        return _color === "#FFFFFF"
+    }
+}
+
+class Elipse extends Figura {
+    constructor(x, y, rh, rv) {
+        super(x, y);
+        this._rh = rh;
+        this._rv = rv;
+    }
+    pintar() {
+        console.log("Nos movemos a la posición (" + str(this._x) + ", " + str(this._y) + ") Cogemos la pintura de color " + this._color /
+            + "Pintamos elipse de radios " + _rh + " y " + _rv)
+    }
+}
+
+
+class Circulo extends Elipse {
+    constructor(x, y, r) {
+        super(x, y, r, r);
+    }
+}
