f77d6a20a3b75c7bd614f9446c56107093349ef2
Victor <victordpc@gmail.com>
2018-11-28 22:25:06 +0100

    Estructura proyecto 1

---
+++ Proyecto/Proyecto01/config.js
@@ -0,0 +1,22 @@
+// config.js
+"use strict";
+
+const session = require("express-session");
+const mysqlSession = require("express-mysql-session");
+const MySQLStore = mysqlSession(session);
+
+module.exports = {
+        mysqlConfig: {
+                host: "localhost", // Ordenador que ejecuta el SGBD
+                user: "root", // Usuario que accede a la BD
+                password: "", // Contraseña con la que se accede a la BD
+                database: "book" // Nombre de la base de datos
+        },
+        port: 3000, // Puerto en el que escucha el servidor
+        sessionStore: new MySQLStore({
+                host: "localhost",
+                user: "root",
+                password: "",
+                database: "book"
+        }),
+}
---
+++ Proyecto/Proyecto01/index.js
@@ -0,0 +1,81 @@
+"use strict"
+
+const config = require("./config");
+const daoUser = require("./DAOUsers");
+const daoPreguntas = require("./DaoPreguntas");
+const path = require("path");
+const mysql = require("mysql");
+const express = require("express");
+const bodyParser = require("body-parser");
+const session = require("express-session");
+const expressValidator = require("express-validator");
+
+// Crear un servidor Express.js
+const app = express();
+
+// Crear un pool de conexiones a la base de datos de MySQL
+const _pool = mysql.createPool(config.mysqlConfig);
+let daoU = new daoUser(_pool);
+let daoP = new daoPreguntas(_pool);
+
+const sessionStore = config.sessionStore;
+// Usamos el midd de sesion
+const middlewareSession = session({
+    saveUninitialized: false,
+    secret: "foobar34",
+    resave: false,
+    store: sessionStore
+});
+app.use(middlewareSession);
+
+// Configuramos el motor de plantillas
+app.set("view engine", "ejs");
+
+// Definimos el directorio de plantillas
+app.set("views", path.join(__dirname, "public", "views"));
+
+// Middleware Static
+const ficherosEstaticos = path.join(__dirname, "public");
+app.use(express.static(ficherosEstaticos));
+
+// Se incluye el middleware body-parser en la cadena de middleware
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+
+// Se incluye el validador de formularios
+app.use(expressValidator());
+
+// Middleware para mensajes flash
+function flashMiddleware(request, response, next) {
+    response.setFlash = function (msg) {
+        request.session.flashMsg = msg;
+    };
+    response.locals.getAndClearFlash = function () {
+        let msg = request.session.flashMsg;
+        delete request.session.flashMsg;
+        return msg;
+    };
+    next();
+};
+app.use(flashMiddleware);
+
+// Arrancar el servidor
+app.listen(config.port, function (err) {
+    if (err) {
+        console.log("ERROR al iniciar el servidor");
+    } else {
+        console.log(`Servidor arrancado en el puerto ${config.port}`);
+    }
+});
+
+
+//Middleware para control de acceso
+function compruebaUsuario(request, response, next) {
+    if (request.session.currentUser) {
+        response.locals.userEmail = request.session.currentUser
+        next();
+    } else {
+        response.redirect('login.html');
+    }
+}
