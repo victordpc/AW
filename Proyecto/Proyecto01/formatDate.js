025bf86b30226703248cc80a4376be267c8a144a
Victor <victordpc@gmail.com>
2019-06-01 18:28:45 +0200

    Perfil y busqueda amigos

--- Proyecto/Proyecto01/formatDate.js
+++ Proyecto/Proyecto01/formatDate.js
@@ -3,7 +3,10 @@
  * Funciones auxiliares
  */
 function formatDate(date) {
-    var d = new Date(date), month = '' + (d.getMonth() + 1), day = '' + d.getDate(), year = d.getFullYear();
+    var d = new Date(date),
+        month = '' + (d.getMonth() + 1),
+        day = '' + d.getDate(),
+        year = d.getFullYear();
     if (month.length < 2)
         month = '0' + month;
     if (day.length < 2)
